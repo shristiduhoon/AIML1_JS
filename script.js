06eab6b3fdf212b7315eb39b7c290d1602e5d653
shristiduhoon <shristiduhoon24@gmail.com>
2026-02-23 15:40:57 +0530

    commit

---
+++ script.js
@@ -0,0 +1,75 @@
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num="Sanjeev soni";
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
+ 
+const sym1=Symbol(4)
+console.log(sym1);
+const sym2=Symbol(4)
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false");//because each symbol is unique
+}
+
+var a=100;var b=13;var c=10;var linebreak="<br>";
+document.write("a+b+c=");
+result=a+b+c;
+document.write(result);
+document.write(linebreak);
+
+var a=10;
+var b=20;
+document.write("(a==b)=");
+result=(a==b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a>=b)=")
+result=(a>=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a<=b)=")
+result=(a<=b)
+document.write(result);
+document.write(linebreak);
+
+document.write("(a===b)=")
+result=(a===b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a!=b)=")
+result=(a!=b);
+document.write(result);
+document.write(linebreak);
+
+// assignment-compare three numbers and max as result with ternary operator
+
+var count;
+document.write("Starting loop");
+document.write(linebreak);
+for(count=0;count<10;count++){
+    document.write(count);
+    document.write("<br/>");
+}
+document.write("loop stopped!");
+
+const person={name:"ajay",lname:"chauhan",age:25};
+for(let x in person){
+    console.log("person details: "+x+"="+person[x]);
+}
