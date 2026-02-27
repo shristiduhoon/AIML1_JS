df8c9ae6acfb11e257039deb2b5f2407ca44de20
shristiduhoon <shristiduhoon24@gmail.com>
2026-02-27 09:49:26 +0530

    func

--- javascriptfunc.js
+++ javascriptfunc.js
@@ -1,3 +1,13 @@
-function myFunction(){
-    alert("Hello World!");
+// function myFunction(){
+//     alert("Hello World!");
+// }
+//  function sayHello(name,age){
+//     document.write(name + "is" + age + "years old.");
+//  }
+// when a function does not return value it becomes undefined insted of saying void 
+
+var res= mul(12,30);
+function mul(x,y){
+    return x*y;
 }
+document.write(res);
