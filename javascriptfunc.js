4907336503c7ab3617756bb297bb08c0d4f41cbc
shristiduhoon <shristiduhoon24@gmail.com>
2026-04-08 22:40:08 +0530

    change

--- javascriptfunc.js
+++ javascriptfunc.js
@@ -1,30 +1,30 @@
-//  function myFunction(){
-//     alert("Hello World!");
-//  }
+ function myFunction(){
+    alert("Hello World!");
+ }
 
-//   function sayHello(name,age){
-//      document.write(name + "is" + age + "years old.");
-//  }
-//  when a function does not return value it becomes undefined insted of saying void 
+   function sayHello(name,age){
+      document.write(name + "is" + age + "years old.");
+  }
+  when a function does not return value it becomes undefined insted of saying void 
 
-//  var res= mul(12,30);
-//  function mul(x,y){
-//      return x*y;
-//  }
-//  document.write(res);
+  var res= mul(12,30);
+  function mul(x,y){
+      return x*y;
+  }
+  document.write(res);
 
-// //Arrow function|
-// var add2=(a,b)=>{
-//     console.log(a+b);
-// }
-// add2(102,20);
+ //Arrow function|
+ var add2=(a,b)=>{
+     console.log(a+b);
+ }
+ add2(102,20);
 
-//Object
-// var person={name:"John mac",age:30,isMarried:true,address:{street:"vijay nagar",flatno:201}};
-// console.log(person);
-// for(let x in person){
-//     console.log("details : "+ x + ": "+ person[x]);
-// }
+Object
+ var person={name:"John mac",age:30,isMarried:true,address:{street:"vijay nagar",flatno:201}};
+ console.log(person);
+ for(let x in person){
+     console.log("details : "+ x + ": "+ person[x]);
+ }
 
 var person2= new Object();
 person2.name='krishnkant';
