7d901adb9820d2998bf48c54ae3c8003f301d071
shristiduhoon <shristiduhoon24@gmail.com>
2026-04-09 22:46:09 +0530

    change

--- practice.js
+++ practice.js
@@ -1,35 +1,37 @@
-function calculateResult(){
-    let n=document.getElementById("subject").value;
-    let i;
-    let total=0;
-    for(i=0;i<n;i++){
-        let x=parseFloat(prompt("Enter the subject marks,"+(i+1)));
-        total+=x;
-    }
-    let avg=total/n;
-    let grade;
-    let resultstatus;
-    if(avg>=90){
-        grade="A+";
-    }
-    else if(avg>=80){
-        grade="b";
-    }
-    else if(avg>=70){
-        grade="c";
-    }
-    else if(avg>=50){
-        grade="d";
-    }
-    else{
-        grade="e";
-    }
-    if(avg>40){
-        resultstatus="pass";
-    }
-    else{
-        resultstatus="fail";
-    }
-    document.getElementById("result").innerHTML="Total marks:"+total+"<br/>"+"average:"+avg.toFixed(2)+"<br/>"
-    +"grade:"+grade+"<br/>"+"resultstatus"+resultstatus;
+let employees=[];
+function addEmployee(){
+    let name=document.getElementById("name").value;
+    let id=document.getElementById("id").value;
+    let sal=parseFloat(document.getElementById("sal").value);
+    let dept=document.getElementById("dept").value;
+    let employee={name:name,id:id,salary:sal,dept:dept};
+    employees.push(employee);
+    alert("employee added succesfully");
+}
+function displayEmployee(){
+    let output="<h3>All employees</h3>";
+    employees.forEach(emp=>{
+        output+=`Name:${emp.name} | ID:${emp.id} | Salary:${emp.sal} | Dept:${emp.dept}<br>`;
+    })
+    document.getElementById("output").innerHTML=output;
+}
+function filterSalary(){
+    let filtered=employees.filter(emp=>emp.sal>50000);
+    let output="<h3>Employees with salary>500000";
+    filtered.forEach(emp=>{
+        output+=`Name:${emp.name} | SALARY:${emp.sal};`
+    })
+    document.getElementById("output").innerHTML=output;
+}
+function totalSalary(){
+    let total = employees.reduce((sum,emp)=>sum+emp.salary,0)
+}
+function averageSalary(){
+    let total=employees.reduce((sum,emp)=>sum+emp.sal,0);
+    let avg=total/employees.length;
+}
+function countDepartment(){
+    let deptName=prompt("Enter department name");
+    let count=employees.filter(emp=>emp.dept.toLowerCase()==emp.deptName.toLowerCase()).length;
+    document.getElementById("output").innerHTML="employees in"+deptName+":"+count;
 }
